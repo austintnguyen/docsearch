78141a306c4711976804d5cbbb5c13797f950458
austintnguyen <atn038@ucsd.edu>
2022-10-20 13:50:22 -0700

    Lab 4 procedures

---
+++ DocSearchTest.java
@@ -0,0 +1,10 @@
+/*public class DocSearchTest extends DocSearchServer{
+
+@Test
+public void testHandleUrl(){
+    assertEquals("Don't know how to handle that path!", handleRequest());
+
+}
+}
+
+*/
